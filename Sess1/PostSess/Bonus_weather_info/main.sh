dfa0ff4035450d07cf81eb8ac2833d60aa6dafd8
Anurag-PyDev <anuragsharma.takethat@gmail.com>
2024-08-16 11:05:34 +0530

    Bonus question done

--- Sess1/PostSess/Bonus_weather_info/main.sh
+++ Sess1/PostSess/Bonus_weather_info/main.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+#Uses the Open weather api to get json data and extract the temerature part of it
+
+echo "Enter the city name : "
+read city
+
+#City names involving whitespaces pose a problem in the url so the whitespacs needs to be replaced with %20 so the url query is passed without problems
+city=${city// /'%20'}
+
+data=$(curl -s "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=4a9fa4138744420bacdfc4e4e1553ddd&units=metric" | jq '.main.temp')
 
 
-curl wttr.in/chennai > data.txt
-
+echo "The temperature in $city is $data degrees Celsius."
 
 
