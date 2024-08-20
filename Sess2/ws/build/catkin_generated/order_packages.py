a287ddd3f91ac8e146b8fd67b0c5c1daa7f33f4f
Anurag-PyDev <anuragsharma.takethat@gmail.com>
2024-08-20 08:48:36 -0700

    Tested ROS publisher and subscriber

---
+++ Sess2/ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/hawcker/auv-stuff/Sess2/ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/hawcker/auv-stuff/Sess2/sample_ws/devel;/home/hawcker/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/hawcker/auv-stuff/Sess2/sample_ws/devel;/home/hawcker/catkin_ws/devel;/opt/ros/noetic' != '' else []
